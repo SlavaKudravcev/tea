bc673dc36939981e916775987df715a13d0d9b13
SlavaKudravcev <dinarowa@rambler.ru>
2024-11-10 00:32:40 +0500

    Updated new-files

--- new-files/main.py
+++ new-files/main.py
@@ -52,3 +52,4 @@ Stay hungry, stay foolish.
 Success usually comes to those who are too busy to be looking for it.
 Opportunities don't happen. You create them.
 Success usually comes to those who are too busy to be looking for it.
+Innovation distinguishes between a leader and a follower.
