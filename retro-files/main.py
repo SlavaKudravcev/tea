c50f2b592cbff472ae805aad0ae05ad235091027
SlavaKudravcev <dinarowa@rambler.ru>
2024-11-19 00:24:04 +0500

    Updated retro-files

--- retro-files/main.py
+++ retro-files/main.py
@@ -31,3 +31,4 @@ Opportunities don't happen. You create them.
 Don't be afraid to give up the good to go for the great.
 The only way to do great work is to love what you do.
 Opportunities don't happen. You create them.
+Success usually comes to those who are too busy to be looking for it.
