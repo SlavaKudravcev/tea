8972aed88de770e10308ca435dd476b50b322842
SlavaKudravcev <dinarowa@rambler.ru>
2024-11-21 00:25:41 +0500

    Updated new-files

--- new-files/main.py
+++ new-files/main.py
@@ -7,3 +7,4 @@ Innovation distinguishes between a leader and a follower.
 The only way to do great work is to love what you do.
 The only way to do great work is to love what you do.
 Innovation distinguishes between a leader and a follower.
+Simplicity is the ultimate sophistication.
