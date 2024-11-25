f01de2ab539272b3cd6a229ebd317832a8784f82
SlavaKudravcev <dinarowa@rambler.ru>
2024-11-26 00:32:38 +0500

    Updated new-files

--- new-files/main.py
+++ new-files/main.py
@@ -46,3 +46,4 @@ Opportunities don't happen. You create them.
 The only way to do great work is to love what you do.
 Innovation distinguishes between a leader and a follower.
 Innovation distinguishes between a leader and a follower.
+Dream big and dare to fail.
