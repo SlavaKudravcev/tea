c13c08553ab6078bf05fabd462edff825c671b53
SlavaKudravcev <dinarowa@rambler.ru>
2024-12-13 00:24:01 +0500

    Updated retro-files

--- retro-files/app.py
+++ retro-files/app.py
@@ -11,3 +11,4 @@ Innovation distinguishes between a leader and a follower.
 Innovation distinguishes between a leader and a follower.
 If you can dream it, you can do it.
 Act as if what you do makes a difference. It does.
+Simplicity is the ultimate sophistication.
--- retro-files/main.py
+++ retro-files/main.py
@@ -20,3 +20,4 @@ The only way to do great work is to love what you do.
 Simplicity is the ultimate sophistication.
 Simplicity is the ultimate sophistication.
 Act as if what you do makes a difference. It does.
+The only way to do great work is to love what you do.
