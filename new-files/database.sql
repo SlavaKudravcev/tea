f31e87c2c91d71e7126947b5983a320a324f25b2
SlavaKudravcev <dinarowa@rambler.ru>
2024-12-14 00:25:40 +0500

    Updated new-files

--- new-files/database.sql
+++ new-files/database.sql
@@ -7,3 +7,4 @@ Opportunities don't happen. You create them.
 Innovation distinguishes between a leader and a follower.
 The only way to do great work is to love what you do.
 Innovation distinguishes between a leader and a follower.
+Simplicity is the ultimate sophistication.
