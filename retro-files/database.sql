29b24d2575d66b6edd3e3cda1bbf4382faf80da2
SlavaKudravcev <dinarowa@rambler.ru>
2024-12-14 00:24:01 +0500

    Updated retro-files

--- retro-files/database.sql
+++ retro-files/database.sql
@@ -11,3 +11,4 @@ Opportunities don't happen. You create them.
 Don't be afraid to give up the good to go for the great.
 Act as if what you do makes a difference. It does.
 If you can dream it, you can do it.
+Innovation distinguishes between a leader and a follower.
