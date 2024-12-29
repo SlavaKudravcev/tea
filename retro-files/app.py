fc232df6dedc9a6e2e4423ce01e21af0cda8f4ce
SlavaKudravcev <dinarowa@rambler.ru>
2024-12-30 00:24:00 +0500

    Updated retro-files

--- retro-files/app.py
+++ retro-files/app.py
@@ -7,3 +7,4 @@ If you can dream it, you can do it.
 The only way to do great work is to love what you do.
 Act as if what you do makes a difference. It does.
 Dream big and dare to fail.
+Innovation distinguishes between a leader and a follower.
--- retro-files/database.sql
+++ retro-files/database.sql
@@ -9,3 +9,4 @@ If you can dream it, you can do it.
 Dream big and dare to fail.
 Opportunities don't happen. You create them.
 Don't be afraid to give up the good to go for the great.
+Act as if what you do makes a difference. It does.
