2e9e8c4dd72e749134bb87b26839183c1c5414a8
SlavaKudravcev <dinarowa@rambler.ru>
2025-01-16 00:23:57 +0500

    Updated retro-files

--- retro-files/database.sql
+++ retro-files/database.sql
@@ -4,3 +4,4 @@ The only way to do great work is to love what you do.
 Act as if what you do makes a difference. It does.
 Opportunities don't happen. You create them.
 Simplicity is the ultimate sophistication.
+Innovation distinguishes between a leader and a follower.
