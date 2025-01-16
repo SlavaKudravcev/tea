341d087fada2466595b8e730f68a84cf8a6ed690
SlavaKudravcev <dinarowa@rambler.ru>
2025-01-17 00:25:38 +0500

    Updated new-files

--- new-files/app.py
+++ new-files/app.py
@@ -1,3 +1,4 @@
 Simplicity is the ultimate sophistication.
 Don't be afraid to give up the good to go for the great.
 Don't be afraid to give up the good to go for the great.
+The only way to do great work is to love what you do.
--- new-files/main.py
+++ new-files/main.py
@@ -1,2 +1,3 @@
 Simplicity is the ultimate sophistication.
 Act as if what you do makes a difference. It does.
+Stay hungry, stay foolish.
