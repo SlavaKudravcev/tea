0cb94d16f86dc040083fba0bb69f2ebe055d23dc
SlavaKudravcev <dinarowa@rambler.ru>
2025-01-26 00:23:56 +0500

    Updated retro-files

--- retro-files/database.sql
+++ retro-files/database.sql
@@ -2,3 +2,4 @@ Dream big and dare to fail.
 Dream big and dare to fail.
 The only way to do great work is to love what you do.
 Act as if what you do makes a difference. It does.
+Opportunities don't happen. You create them.
--- retro-files/script.js
+++ retro-files/script.js
@@ -6,3 +6,4 @@ Dream big and dare to fail.
 Don't be afraid to give up the good to go for the great.
 Dream big and dare to fail.
 Simplicity is the ultimate sophistication.
+Dream big and dare to fail.
