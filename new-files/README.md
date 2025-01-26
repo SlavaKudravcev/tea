7d41afe3f6457b151e66a5c2f4ce6761735dc960
SlavaKudravcev <dinarowa@rambler.ru>
2025-01-27 00:25:37 +0500

    Updated new-files

--- new-files/README.md
+++ new-files/README.md
@@ -1,3 +1,4 @@
 Success usually comes to those who are too busy to be looking for it.
 Innovation distinguishes between a leader and a follower.
 Don't be afraid to give up the good to go for the great.
+Stay hungry, stay foolish.
--- new-files/database.sql
+++ new-files/database.sql
@@ -2,3 +2,4 @@ Stay hungry, stay foolish.
 If you can dream it, you can do it.
 Act as if what you do makes a difference. It does.
 The only way to do great work is to love what you do.
+Opportunities don't happen. You create them.
